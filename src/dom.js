048c738d63910fdeecf2588ac4a3a5bce704a23b
Neil Douglas <endy64@icloud.com>
2022-07-28 17:59:44 +0100

    Edited CSS so board keep aspect ratio and is responsive for mobile

--- src/dom.js
+++ src/dom.js
@@ -1,16 +1,17 @@
 const drawBoard = (player) => {
   const board = document.querySelector(`.${player} .board`);
   for (let i = 0; i < 10; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+    // const row = document.createElement('div');
+    // row.classList.add('row');
     for (let j = 0; j < 10; j++) {
       const div = document.createElement('div');
       div.dataset.y = i;
       div.dataset.x = j;
       div.classList.add('square');
-      row.appendChild(div);
+      // row.appendChild(div);
+      board.appendChild(div);
     }
-    board.appendChild(row);
+    // board.appendChild(row);
   }
 };
 
